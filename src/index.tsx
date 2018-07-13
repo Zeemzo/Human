76da74928f9badb08072ac9c412d13882ea36e4e
harsh217 <harsh_217@yahoo.com>
2018-07-13 19:29:07 +0530

    Azeem 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import App2 from './App2';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
+);
+
+ReactDOM.render(
+  <App2 />,
+  document.getElementById('root1') as HTMLElement
 );
 registerServiceWorker();
