226bb40b3f8d4765de6f6a7789c2baee4f8afa8e
Zeemzo <azeemashraf@outlook.com>
2018-08-31 09:50:12 +0530

    1st edit

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,19 +1,38 @@
 import * as React from 'react';
+import { Button,ButtonToolbar } from 'react-bootstrap';
 import './App.css';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 class App2 extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Go Home And Be Human</h1>
-        </header>
-      </div>
+      <ButtonToolbar>
+  {/* Standard button */}
+  <Button>Default</Button>
+
+  {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+  <Button bsStyle="primary">Primary</Button>
+
+  {/* Indicates a successful or positive action */}
+  <Button bsStyle="success">Success</Button>
+
+  {/* Contextual button for informational alert messages */}
+  <Button bsStyle="info">Info</Button>
+
+  {/* Indicates caution should be taken with this action */}
+  <Button bsStyle="warning">Warning</Button>
+
+  {/* Indicates a dangerous or potentially negative action */}
+  <Button bsStyle="danger">Danger</Button>
+
+  {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+  <Button bsStyle="link">Link</Button>
+</ButtonToolbar>
+
     );
   }
 }
+
 
 export default App2;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import App2 from './App2';
+import App3 from './App3';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -13,5 +14,10 @@ ReactDOM.render(
 ReactDOM.render(
   <App2 />,
   document.getElementById('root1') as HTMLElement
+);
+
+ReactDOM.render(
+  <App3 />,
+  document.getElementById('root2') as HTMLElement
 );
 registerServiceWorker();
